38008347a92ed7d6a1a6d368877efcd7809b3f1e
vignesh4github <csevickee@gmail.com>
2017-07-20 09:33:28 -0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname  + '/views/partials')
@@ -65,7 +66,7 @@ app.get('/bad', (req,res) =>
 
 });
 
-app.listen(3000,() =>
+app.listen(port,() =>
 {
-  console.log('Server is now running on Local host : 3000');
+  console.log(`Server is now running on Local host ${port}`);
 });
